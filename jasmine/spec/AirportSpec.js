bae6676921d3c93b46b4681037568c1eb4a5b1aa
Chris Cooper <cjcooper252@gmail.com>
2016-12-06 15:22:48 +0000

    checks plane is in airport

--- jasmine/spec/AirportSpec.js
+++ jasmine/spec/AirportSpec.js
@@ -22,11 +22,22 @@ var plane;
       expect(plane.land).toHaveBeenCalled();
     });
 
+    it("should have the plane following landing", function() {
+      airport.land(plane);
+      expect(airport.planes).toContain(plane);
+    });
+
     it("should be able to take off a plane", function() {
       spyOn(plane,'takeOff');
       airport.land(plane);
       airport.takeOff(plane);
       expect(plane.takeOff).toHaveBeenCalled();
+    });
+
+    it("should not have the plane following take off", function() {
+      airport.land(plane);
+      airport.takeOff(plane);
+      expect(airport.planes).not.toContain(plane);
     });
 
 });
--- jasmine/src/Airport.js
+++ jasmine/src/Airport.js
@@ -7,8 +7,21 @@ function Airport() {
 
 Airport.prototype.land = function(plane) {
   plane.land();
+  this.addPlane(plane);
 };
 
 Airport.prototype.takeOff = function(plane) {
   plane.takeOff();
+  this.removePlane(plane)
+};
+
+Airport.prototype.addPlane = function(plane) {
+  this.planes.push(plane);
+};
+
+Airport.prototype.removePlane = function(plane) {
+  var index = this.planes.indexOf(plane);
+  if (index > -1) {
+    this.planes.splice(index,1);
+  };
 };
